010c14cb9aed262ac15c05f1a16168ac8cc9e9f5
BOSSECODE <eduarda_b_miranda@estudante.sesisenai.org.br>
2026-03-11 09:21:18 -0300

    ADD-Função

---
+++ script.js
@@ -0,0 +1,47 @@
+// console.log("Hello world!");
+
+// let nome = "João";
+// console.log(nome);
+
+// const PI = 3.14;
+// console.log(PI);
+
+// let n1 = 1;
+// let n2 = 2;
+
+// console.log(n1 + n2);
+
+// function saudacao() {
+//     console.log("Olá, Meu nome é " + nome);
+// };
+
+// function somar(n3,n4){
+//     console.log(n3 + n4);
+// };
+
+// saudacao();
+// somar(1, 1);
+
+
+function somarnumeros(){
+    
+    let soma = 0;
+    let continuar = true;
+
+    while(continuar === true){
+        let numero = parseFloat(prompt("Digite um número (ou digite '0' para sair) :"));
+
+        // soma = soma + numero;
+        // soma += numero;
+
+        if (isNaN(numero)) {
+            alert("Por favor, digite um número válido!");
+        } else {
+            soma += numero;
+            console.log(soma);
+        }
+        continuar = confirm("Deseja adicionar mais um número?");
+    }
+    alert("A soma dos numeros é : " + soma);
+}
+somarnumeros();
